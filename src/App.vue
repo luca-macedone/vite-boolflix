8933452a934ab1dd08d93155f6fc5258fb4d6e3a
luca-macedone <luca.macedone@outlook.it>
2023-04-19 11:45:07 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <h1>hello</h1>
+</template>
+
+<style lang="scss" scoped>
+@use './assets/sass/app.scss' as *;
+</style>
