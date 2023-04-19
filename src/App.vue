b2d90e5d2c4cedf15fcb4a2703c2867c9991d279
luca-macedone <luca.macedone@outlook.it>
2023-04-19 12:40:20 +0200

    add global state functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <script>
+import { state } from './state';
 export default {
   data() {
     return {
-
+      state,
     }
-  }
+  },
+  methods: {
+    filteredSearch() {
+      this.state.fetchMovies(this.state.setURL())
+    }
+  },
+  // mounted() {
+  //   this.state.fetchMovies(this.state.setURL()) 
+  // },
 }
 </script>
 
 <template>
-  <h1>hello</h1>
+  <header>
+    <input type="text" v-model="state.query">
+    <button @click="filteredSearch()">Cerca</button>
+  </header>
+  <main>
+    <div class="card" v-for="movie in state.movieList">
+      {{ movie.original_title }}
+    </div>
+  </main>
 </template>
 
 <style lang="scss" scoped>
-@use './assets/sass/app.scss' as *;
+@use './assets/sass/app.scss';
 </style>
